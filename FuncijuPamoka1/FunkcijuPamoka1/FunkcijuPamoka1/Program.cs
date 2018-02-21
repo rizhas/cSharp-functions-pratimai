9ee610dce49aefad0baa15b164fa38f20d3e3f78
Unknown <rizhas69@gmail.com>
2018-02-21 19:35:18 +0200

    Git sukurimas.
    Tuschias projektas.

---
+++ FuncijuPamoka1/FunkcijuPamoka1/FunkcijuPamoka1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunkcijuPamoka1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
